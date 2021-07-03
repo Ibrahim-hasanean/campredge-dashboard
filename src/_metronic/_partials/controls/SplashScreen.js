0f6d9901b24f01a19186218740f1be43bbaf31a8
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-07-03 20:52:02 +0300

    use the new logo

--- src/_metronic/_partials/controls/SplashScreen.js
+++ src/_metronic/_partials/controls/SplashScreen.js
@@ -1,15 +1,12 @@
 import React from "react";
-import {CircularProgress} from "@material-ui/core";
-import {toAbsoluteUrl} from "../../_helpers";
+import { CircularProgress } from "@material-ui/core";
+import { toAbsoluteUrl } from "../../_helpers";
 
 export function SplashScreen() {
   return (
     <>
       <div className="splash-screen">
-        <img
-          src={toAbsoluteUrl("/media/logos/logo.jpeg")}
-          alt="Al hawat logo"
-        />
+        <img src={toAbsoluteUrl("/media/logos/logo.png")} alt="Taxi logo" />
         <CircularProgress className="splash-screen-spinner" />
       </div>
     </>
