f1bf21658c7244ff38cb067a9c7afe8a6d80c3e0
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-07-10 16:32:06 +0300

    fix snackbar style

--- src/_metronic/layout/components/CustomSnackbar/style.js
+++ src/_metronic/layout/components/CustomSnackbar/style.js
@@ -5,15 +5,6 @@ export const useStyles = makeStyles(theme => ({
     "&.MuiAlert-filledError": {
       backgroundColor: "red"
     },
-    "& > .MuiAlert-icon": {
-      margin: 0,
-      marginLeft: "12px"
-    },
-    "& > .MuiAlert-action": {
-      margin: 0,
-      padding: 0,
-      marginRight: "12px"
-    },
     "& > .MuiAlert-message": {
       padding: "11px 0"
     }
