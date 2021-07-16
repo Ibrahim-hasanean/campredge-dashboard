a5a40c9e6b0f213c5a13984461b781846de8b53f
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-07-16 18:58:05 +0300

    remove button styles

--- src/_metronic/layout/components/CustomButton/style.js
+++ src/_metronic/layout/components/CustomButton/style.js
@@ -2,12 +2,6 @@ import makeStyles from "@material-ui/core/styles/makeStyles";
 
 export const useStyles = makeStyles(theme => ({
   button: {
-    color: "#fff",
-    fontSize: "1.2rem",
-    fontWeight: "bold",
-    "& > .MuiButton-label > .MuiButton-endIcon": {
-      margin: 0,
-      marginRight: "4px"
-    }
+    color: "#fff"
   }
 }));
