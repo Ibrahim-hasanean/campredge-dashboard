33fc30fc8cc60ee561d07f857959dc794ab827b6
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-07-17 17:22:20 +0300

    adjust UserInfo component

--- src/_metronic/layout/components/UsersTableActions/components/UserDetails/components/UserInfo/index.js
+++ src/_metronic/layout/components/UsersTableActions/components/UserDetails/components/UserInfo/index.js
@@ -3,11 +3,10 @@ import { Grid, Typography } from "@material-ui/core";
 import CustomCard from "_metronic/layout/components/CustomCard";
 import Avatar from "_metronic/layout/components/Avatar";
 import CustomList from "_metronic/layout/components/CustomList";
-
 import {
   getUserFullName,
   getFirstCharactersOfUserName,
-  getUsersInfoList
+  getUserInfoList
 } from "helpers/generalHelpers";
 import { useStyles } from "./style";
 
@@ -25,11 +24,11 @@ const UserInfo = ({ userData }) => {
           >
             {getFirstCharactersOfUserName(userData)}
           </Avatar>
-          <Typography align="left" variant="subtitle1">
+          <Typography variant="subtitle1">
             {getUserFullName(userData)}
           </Typography>
         </Grid>
-        <CustomList items={getUsersInfoList(userData)} dense />
+        <CustomList items={getUserInfoList(userData)} dense />
       </CustomCard>
     </Grid>
   );
