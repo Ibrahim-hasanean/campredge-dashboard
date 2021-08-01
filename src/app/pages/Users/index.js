6e294b8a28ede4f8f2fd023b7697bb8ac37286a0
Mohammed Jaber <mohjbr.dev@gmail.com>
2021-08-01 23:22:49 +0300

    Adjust users page to adapt the new filter and users data

--- src/app/pages/Users/index.js
+++ src/app/pages/Users/index.js
@@ -1,22 +1,17 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Box } from "@material-ui/core";
 import UsersFilterControls from "_metronic/layout/components/UsersFilterControls";
-import RoundedCards from "_metronic/layout/components/RoundedCards";
 import CustomTable from "_metronic/layout/components/CustomTable";
 import Snackbar from "_metronic/layout/components/CustomSnackbar";
 import { getUsers } from "api/Users";
 import { API_COMMON_STATUS } from "helpers/api-helper";
 
-const initailUserDataState = {
-  sellersNumber: 0,
-  totalNumber: 0,
-  users: [],
-  usersNumber: 0
-};
 const Users = () => {
-  const [usersData, setUsersData] = useState(initailUserDataState);
+  const [usersData, setUsersData] = useState(null);
   const [hasError, setHasError] = useState(false);
   const [query, setQuery] = useState("");
+  const [startFromZero, setStartFromZero] = useState(true);
+  const prevDataOffset = useRef(0);
 
   useEffect(() => {
     updateTableData(query);
@@ -31,19 +26,30 @@ const Users = () => {
   };
 
   const updateTableData = query => {
-    setUsersData({});
+    if (query.indexOf("offset") === -1) {
+      setStartFromZero(prevState => !prevState);
+    }
+    setUsersData(null);
     getUsers(query)
       .then(response => {
         console.log("test users data", response);
         if (response.responseStatus === API_COMMON_STATUS.SUCCESS) {
-          setUsersData(response.users);
+          if (prevDataOffset.current) {
+            setUsersData(prevUsersData => [
+              ...prevUsersData,
+              ...response.users
+            ]);
+            prevDataOffset.current += response.users.length;
+          } else {
+            setUsersData(response.users);
+          }
         } else {
           setHasError(true);
-          setUsersData(initailUserDataState);
+          setUsersData([]);
         }
       })
       .catch(error => {
-        setUsersData(initailUserDataState);
+        setUsersData([]);
         setHasError(true);
         console.log(error);
         console.error(error);
@@ -51,16 +57,49 @@ const Users = () => {
   };
 
   const updateOrdersQuery = filterData => {
-    const { userName, userType } = filterData;
-    let filterQuery = "";
-    if (userName) {
-      filterQuery = `name=${userName}`;
+    const {
+      firstName,
+      secondName,
+      phoneNumber,
+      ordersNumber,
+      verified,
+      suspend
+    } = filterData;
+    console.log(filterData);
+    let filterQuery = "skip=20";
+
+    if (firstName) {
+      filterQuery = `firstName=${firstName}`;
     }
 
-    if (userType !== "الكل") {
+    if (secondName) {
       filterQuery = filterQuery
-        ? `${filterQuery}&type=${userType}`
-        : `type=${userType}`;
+        ? `${filterQuery}&secondName=${secondName}`
+        : `secondName=${secondName}`;
+    }
+
+    if (phoneNumber) {
+      filterQuery = filterQuery
+        ? `${filterQuery}&phoneNumber=${phoneNumber}`
+        : `phoneNumber=${phoneNumber}`;
+    }
+
+    if (ordersNumber) {
+      filterQuery = filterQuery
+        ? `${filterQuery}&ordersNumber=${ordersNumber}`
+        : `ordersNumber=${ordersNumber}`;
+    }
+
+    if (verified) {
+      filterQuery = filterQuery
+        ? `${filterQuery}&verified=${verified === "verified" ? true : false}`
+        : `verified=${verified === "verified" ? true : false}`;
+    }
+
+    if (suspend) {
+      filterQuery = filterQuery
+        ? `${filterQuery}&suspend=${suspend === "suspended" ? true : false}`
+        : `suspend=${suspend === "suspended" ? true : false}`;
     }
 
     console.log("test users data", filterData, filterQuery);
@@ -71,48 +110,25 @@ const Users = () => {
     setQuery("");
   };
 
-  const CARDS_DATA = useMemo(
-    () => [
-      {
-        title: "عدد البائعين",
-        value: usersData?.sellersNumber,
-        className: "acceptedCard",
-        id: 1
-      },
-      {
-        title: "الرقم الاجمالي",
-        value: usersData?.totalNumber,
-        className: "totalCard",
-        id: 2
-      },
-      {
-        title: "عدد المستخدمين",
-        value: usersData?.usersNumber,
-        className: "deliveredCard",
-        id: 3
-      }
-    ],
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [usersData]
-  );
-
   return (
     <Box height="100%" style={{ backgroundColor: "#fff", padding: "5px" }}>
       <UsersFilterControls
         onSearchClicked={updateOrdersQuery}
         onResetClicked={resetFilterOrdersData}
       />
-      <RoundedCards data={CARDS_DATA} users />
       <CustomTable
-        data={usersData.users}
+        data={usersData}
         updateTableData={() => updateTableData(query)}
         usersTable
+        startFromFirstPage={startFromZero}
+        updateQuery={setQuery}
+        ref={prevDataOffset}
       />
       <Snackbar
         open={!!hasError}
         handleClose={handleClose}
         type="error"
-        text="حدث خطأ ما يرجى المحاولة مرة اخرى"
+        text="Something went wrong please try again"
       />
     </Box>
   );
